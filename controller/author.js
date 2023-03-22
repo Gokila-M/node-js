0b208b1860d4e0dbc4efa284a26b4c0978849066
Gokila-M <gokila1305@gmail.com>
2023-03-22 17:15:10 +0530

    CRU completed in Author model

--- controller/author.js
+++ controller/author.js
@@ -19,6 +19,39 @@ export const authorReg = async (req, res) => {
         res.status(201).json({ message:"Register Sucessfully"});
       } catch (error) {
         res.status(400).json({ message:error.message });
-      }
-    
+      }    
   };
+  export const authorUpdate = async (req, res) => {
+    let  id  = req.query.id;
+    const exauthor = await Author.findOne({_id:id});
+    try {
+      if (!id) {
+        return res.status(400).json({ message: "Please Provide A Id In Query" });
+      }
+      if (!exauthor) {
+        return res.status(400).json({ message: "Author Not Found" });
+      }   
+      await Author.findByIdAndUpdate({ _id: id },{$set: req.body},{ new: true });
+      return res.status(200).json({ message: "Author Data Updated Successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+  export const getAllAuthor = async (req, res) => {
+    try {
+       const author = await Author.find({})
+       return res.status(200).json({ data:author });
+     } catch (error) {
+       return res.status(500).json({ message: error.message });
+     }
+  };
+ export const getuserById = async (req, res) => {
+     let id = req.params.id
+     try {
+       const author = await Author.findById({ _id:id });
+       if (!author) return res.status(404).json({ message: "Author Not Found" });
+       res.status(200).json({ data: author });
+     } catch (error) {
+       res.status(400).json({ message: error.message });
+     }
+ };
