c15db9e6452b3755ad732f9e18476f848f4c6a5e
Gokila-M <gokila1305@gmail.com>
2023-03-27 15:23:53 +0530

    book , mapping controller

--- controller/book.js
+++ controller/book.js
@@ -16,6 +16,7 @@ export const bookReg = async (req, res) => {
     if (exbookCode) return res.status(400).json({ message: "Book No already exists" });  
     let author= await Author.findOne({ _id:authorId});    
     if (!author) return res.status(400).json({ message: "Author Not Found ...!" });    
+    try {
       let register = new Book({       
         bookName: bookName,       
         bookType: bookType,
@@ -23,7 +24,6 @@ export const bookReg = async (req, res) => {
         stock: stock,
         authorId:authorId      
       });
-      try {
         let Author = await register.save();
         res.status(201).json({ message:"Register Sucessfully"});
       } catch (error) {
@@ -31,3 +31,37 @@ export const bookReg = async (req, res) => {
       }
     
   };
+  export const bookUpdate = async (req, res) => {
+    let  id  = req.query.id;
+    const exbook = await Book.findOne({_id:id});
+    try {
+      if (!id) {
+        return res.status(400).json({ message: "Please Provide A Id In Query" });
+      }
+      if (!exbook) {
+        return res.status(400).json({ message: "Book Not Found" });
+      }   
+      await Book.findByIdAndUpdate({ _id: id },{$set: req.body},{ new: true });
+      return res.status(200).json({ message: "Book Data Updated Successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+  export const getAllbook = async (req, res) => {
+    try {
+       const book = await Book.find({})
+       return res.status(200).json({ data:book });
+     } catch (error) {
+       return res.status(500).json({ message: error.message });
+     }
+  };
+ export const getbookById = async (req, res) => {
+     let id = req.params.id
+     try {
+       const book = await Book.findById({ _id:id });
+       if (!book) return res.status(404).json({ message: "Book Not Found" });
+       res.status(200).json({ data: book });
+     } catch (error) {
+       res.status(400).json({ message: error.message });
+     }
+ };
--- controller/mapping.js
+++ controller/mapping.js
@@ -22,3 +22,37 @@ export const bookuserMapping = async (req, res) => {
    }
    
 };
+export const mappingUpdate = async (req, res) => {
+  let  id  = req.query.id;
+  const exmap = await UserbookMapping.findOne({_id:id});
+  try {
+    if (!id) {
+      return res.status(400).json({ message: "Please Provide A Id In Query" });
+    }
+    if (!exmap) {
+      return res.status(400).json({ message: "Mapping Not Found" });
+    }   
+    await UserbookMapping.findByIdAndUpdate({ _id: id },{$set: req.body},{ new: true });
+    return res.status(200).json({ message: "Mapping Data Updated Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+export const getAllmappings = async (req, res) => {
+  try {
+     const mapping = await UserbookMapping.find({})
+     return res.status(200).json({ data:mapping });
+   } catch (error) {
+     return res.status(500).json({ message: error.message });
+   }
+};
+export const getmappingById = async (req, res) => {
+   let id = req.params.id
+   try {
+     const mapping = await UserbookMapping.findById({ _id:id });
+     if (!mapping) return res.status(404).json({ message: "Mapping Not Found" });
+     res.status(200).json({ data: mapping });
+   } catch (error) {
+     res.status(400).json({ message: error.message });
+   }
+};
